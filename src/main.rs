5c28ee782fa59d806d9bff3ff44556ce4659ca1b
Nathaniel Fernandes <nathaniel.s.fernandes@gmail.com>
2021-09-25 10:37:32 -0400

    right before bvh stuff

--- src/main.rs
+++ src/main.rs
@@ -56,14 +56,17 @@ fn load_image(image_path: &str, rotation: Rotate) -> Arc<DynamicImage> {
 }
 
 fn main() {
-    let lookfrom = Vec3::new(478.0, 278.0, -600.0);
-    let lookat = Vec3::new(278.0, 278.0, 20.0);
+    // let bobert_png: Arc<DynamicImage> = load_image("bobert.png", Rotate::R270);
+    // let bobert_png_1: Arc<DynamicImage> = load_image("bobert.png", Rotate::R180);
+
+    let lookfrom = Vec3::new(278.0, 278.0, -800.0);
+    let lookat = Vec3::new(278.0, 278.0, 0.0);
     let camera = Camera::new(
         lookfrom,
         lookat,
         Vec3::new(0.0, 1.0, 0.0),
         40.0,
-        16.0 / 9.0,
+        1.0, //16.0 / 9.0,
         0.0,
         100.0,
         0.0,
@@ -72,120 +75,108 @@ fn main() {
 
     let mut world: Vec<Object> = Vec::new();
 
-    // world.push(Sphere::new(
-    //     Vec3::new(0.0, -1000.0, -1.0),
-    //     1000.0,
-    //     // Lambertian::new(Image::new("bobert.png".to_string())),
-    //     Metal::new(
-    //         CheckerBoard::new(color!(0.0, 0.0, 0.0), color!(1.0, 1.0, 1.0), 20.0),
-    //         0.2,
-    //     ),
-    // ));
+    //green
+    world.push(Plane::new(
+        PlaneType::YZ,
+        0.0,
+        555.0,
+        0.0,
+        555.0,
+        555.0,
+        Lambertian::new(SolidColor::new(color!(0.12, 0.45, 0.15))),
+        //Lambertian::new(Image::new(bobert_png_1.clone())),
+    ));
 
-    // world.push(Sphere::new(
-    //     Vec3::new(0.0, 3.0, -1.0),
-    //     0.5,
-    //     EmissiveDiffuse::new(SolidColor::new(color!(1.0, 1.0, 1.0))), // Dielectric::new(1.2)
-    //                                                                   //Metal::new(Image::new("earthmap.jpg".to_string()), 0.2),
-    // ));
+    //red
+    world.push(Plane::new(
+        PlaneType::YZ,
+        0.0,
+        555.0,
+        0.0,
+        555.0,
+        0.0,
+        Lambertian::new(SolidColor::new(color!(0.65, 0.05, 0.05))),
+        //Lambertian::new(Image::new(bobert_png_1.clone())),
+    ));
 
-    // world.push(Sphere::new(
-    //     Vec3::new(0.0, 1.0, 0.0),
-    //     1.0,
-    //     // Lambertian::new(Image::new("bobert.png".to_string())),
-    //     Dielectric::new(1.2),
-    // ));
+    //white walls
+    world.push(Plane::new(
+        PlaneType::ZX,
+        0.0,
+        555.0,
+        0.0,
+        555.0,
+        0.0,
+        Lambertian::new(SolidColor::new(color!(0.73, 0.73, 0.73))),
+        //Lambertian::new(Image::new(bobert_png_1.clone())),
+    ));
 
-    // (-6..6).for_each(|a| {
-    //     (-6..6).for_each(|b| {
-    //         let cmat = random_distribution();
-    //         let center = Vec3::new(
-    //             a as f64 + 0.9 * random_distribution(),
-    //             0.2,
-    //             b as f64 + 0.9 * random_distribution(),
-    //         );
+    world.push(Plane::new(
+        PlaneType::ZX,
+        0.0,
+        555.0,
+        0.0,
+        555.0,
+        555.0,
+        Lambertian::new(SolidColor::new(color!(0.73, 0.73, 0.73))),
+        //Lambertian::new(Image::new(bobert_png_1.clone())),
+    ));
 
-    //         if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-    //             if cmat < 0.8 {
-    //                 let albedo = color!(
-    //                     random_distribution(),
-    //                     random_distribution(),
-    //                     random_distribution()
-    //                 ) * color!(
-    //                     random_distribution(),
-    //                     random_distribution(),
-    //                     random_distribution()
-    //                 );
+    world.push(Plane::new(
+        PlaneType::XY,
+        0.0,
+        555.0,
+        0.0,
+        555.0,
+        555.0,
+        Lambertian::new(SolidColor::new(color!(0.73, 0.73, 0.73))),
+        //Lambertian::new(Image::new(bobert_png.clone())), //Lambertian::new(SolidColor::new(color!(0.73, 0.73, 0.73))),
+    ));
 
-    //                 let albedo = SolidColor::new(albedo);
-    //                 // world.push(Sphere::new(center, 0.2, Lambertian::new(albedo)))
-    //                 world.push(Sphere::new(center, 0.2, EmissiveDiffuse::new(albedo)))
-    //             } else if cmat < 0.95 {
-    //                 let albedo = color!(
-    //                     random_float(0.5, 1.0),
-    //                     random_float(0.5, 1.0),
-    //                     random_float(0.5, 1.0)
-    //                 );
-    //                 let albedo = SolidColor::new(albedo);
-    //                 let fuzz = random_float(0.0, 0.5);
+    //light
+    world.push(Plane::new(
+        PlaneType::ZX,
+        213.0,
+        343.0,
+        227.0,
+        332.0,
+        554.0,
+        EmissiveDiffuse::new(SolidColor::new(color!(15.0, 15.0, 15.0))),
+    ));
 
-    //                 world.push(Sphere::new(center, 0.2, Metal::new(albedo, fuzz)))
-    //             } else {
-    //                 world.push(Sphere::new(center, 0.2, Dielectric::new(1.5)))
-    //             }
-    //         }
-    //     });
-    // });
+    create_box(
+        Vec3::new(130.0, 0.0, 65.0),
+        Vec3::new(295.0, 165.0, 230.0),
+        Lambertian::new(SolidColor::new(color!(0.73, 0.73, 0.73))),
+    )
+    .into_iter()
+    .for_each(|plane| world.push(plane));
 
-    (-5..5).for_each(|i| {
-        (-5..5).for_each(|j| {
-            let w = 100.0;
-            let x0 = -1000.0 + i as f64 * w;
-            let z0 = -1000.0 + j as f64 * w;
+    let img = camera.threaded_render(50, &world, &color!(0.0, 0.0, 0.0), 800, 2, 50, None);
+    img.save("test.png").unwrap();
 
-            let y0 = 0.0;
-            let x1 = x0 + w;
-            let y1 = random_float(1.0, 101.0);
-            let z1 = z0 + w;
-
-            create_box(
-                Vec3::new(x0, y0, z0),
-                Vec3::new(x1, y1, z1),
-                Lambertian::new(SolidColor::new(color!(1.0, 0.73, 0.73))),
-            )
-            .into_iter()
-            .for_each(|plane| world.push(plane));
-        });
-    });
-
-    // world.push(Sphere::new(
-    //     Vec3::new(278.0, 140.0, 400.0),
-    //     80.0,
-
-    //     Lambertian::new(SolidColor::new(color!(0.73, 0.73, 0.73)))
-    //     // EmissiveDiffuse::new(Image::new("bobert.png".to_string())), // Dielectric::new(1.2),
-    //     //Dielectric::new(1.2),
-    //     //Metal::new(SolidColor::new(color!(1.0, 1.0, 1.0)), 0.0),
-    // ));
-
-    // let denoise_settings = DenoiseSettings {
-    //     srgb: false,
-    //     hdr: true,
-    //     clean_aux: true,
-    // };
-
-    // let img = camera.threaded_render(
-    //     50,
-    //     &world,
-    //     &color!(0.7, 0.7, 0.7),
-    //     800,
-    //     8,
-    //     50,
-    //     Some(denoise_settings),
-    // );
-    // img.save("test.png").unwrap();
-
-    let (albedo_img, normal_img) = camera.render_buffers(&world, &color!(0.0, 0.0, 0.0), 200);
+    //let (albedo_img, normal_img) = camera.render_buffers(&world, &color!(0.0, 0.0, 0.0), 200);
     //albedo_img.save("albedo.png").unwrap();
-    normal_img.save("normal.png").unwrap();
+    // normal_img.save("normal.png").unwrap();
 }
+
+// (-5..5).for_each(|i| {
+//     (-5..5).for_each(|j| {
+//         let w = 100.0;
+//         let x0 = -1000.0 + i as f64 * w;
+//         let z0 = -1000.0 + j as f64 * w;
+
+//         let y0 = 0.0;
+//         let x1 = x0 + w;
+//         let y1 = random_float(1.0, 101.0);
+//         let z1 = z0 + w;
+
+//         create_box(
+//             Vec3::new(x0, y0, z0),
+//             Vec3::new(x1, y1, z1),
+//             Lambertian::new(SolidColor::new(color!(1.0, 0.73, 0.73))),
+//         )
+//         .into_iter()
+//         .for_each(|plane| world.push(plane));
+//     });
+// });
--- src/objects.rs
+++ src/objects.rs
@@ -1,6 +1,12 @@
+use bvh::aabb::{Bounded, AABB};
+use bvh::bounding_hierarchy::{BHShape, BoundingHierarchy};
+use bvh::bvh::BVH;
+use bvh::{Point3, Vector3};
+
 // use std::cell::RefCell;
 // use std::cmp::Ordering;
 use std::f32::consts::PI;
+use std::ops::Bound;
 // use std::ptr::null;
 
 // use crate::color::Color;
@@ -19,8 +25,8 @@ const PI_F32: f64 = PI as f64;
 #[derive(Clone, Debug)]
 pub enum Object {
     Sphere(Sphere),
-    MovingSphere(MovingSphere),
-    Plane(Plane),
+    // MovingSphere(MovingSphere),
+    // Plane(Plane),
 }
 pub trait Intersectable {
     fn intersects(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<f64>;
@@ -37,9 +43,9 @@ impl Object {
     pub fn material(&self) -> &Material {
         match *self {
             Object::Sphere(ref obj) => &obj.material,
-            Object::MovingSphere(ref obj) => &obj.material,
+            // Object::MovingSphere(ref obj) => &obj.material,
             //Object::BVHNode(ref _obj) => None,
-            Object::Plane(ref obj) => &obj.material,
+            // Object::Plane(ref obj) => &obj.material,
         }
     }
     // }
@@ -48,8 +54,8 @@ impl Object {
     pub fn intersects(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<f64> {
         match *self {
             Object::Sphere(ref obj) => obj.intersects(ray, t_min, t_max),
-            Object::MovingSphere(ref obj) => obj.intersects(ray, t_min, t_max),
-            Object::Plane(ref obj) => obj.intersects(ray, t_min, t_max),
+            // Object::MovingSphere(ref obj) => obj.intersects(ray, t_min, t_max),
+            // Object::Plane(ref obj) => obj.intersects(ray, t_min, t_max),
         }
         // let intersect = match *self {
         //     Object::Sphere(ref obj) => obj.intersects(ray, t_min, t_max),
@@ -67,27 +73,27 @@ impl Object {
     pub fn surface_normal(&self, point: &Vec3, ray: &Ray) -> Vec3 {
         match *self {
             Object::Sphere(ref obj) => obj.surface_normal(point, ray),
-            Object::MovingSphere(ref obj) => obj.surface_normal(point, ray),
+            // Object::MovingSphere(ref obj) => obj.surface_normal(point, ray),
             //    Object::BVHNode(ref obj) => obj.surface_normal(point),
-            Object::Plane(ref obj) => obj.surface_normal(point, ray),
+            // Object::Plane(ref obj) => obj.surface_normal(point, ray),
         }
     }
 
     pub fn surface_uv(&self, point: &Vec3) -> (f64, f64) {
         match *self {
             Object::Sphere(ref obj) => obj.surface_uv(point),
-            Object::MovingSphere(ref obj) => obj.surface_uv(point),
+            // Object::MovingSphere(ref obj) => obj.surface_uv(point),
             //    Object::BVHNode(ref obj) => obj.surface_normal(point),
-            Object::Plane(ref obj) => obj.surface_uv(point),
+            // Object::Plane(ref obj) => obj.surface_uv(point),
         }
     }
 
     pub fn outward_normal(&self, point: &Vec3, time: f64) -> Vec3 {
         match *self {
             Object::Sphere(ref obj) => obj.outward_normal(point, time),
-            Object::MovingSphere(ref obj) => obj.outward_normal(point, time),
+            // Object::MovingSphere(ref obj) => obj.outward_normal(point, time),
             //   Object::BVHNode(ref obj) => obj.outward_normal(point, time),
-            Object::Plane(ref obj) => obj.outward_normal(point, time),
+            // Object::Plane(ref obj) => obj.outward_normal(point, time),
         }
     }
 
@@ -100,6 +106,18 @@ impl Object {
     //     }
     // }
 }
+
+impl Bounded for Object {
+    fn aabb(&self) -> AABB {
+        match *self {
+            Object::Sphere(ref obj) => obj.aabb(),
+            // Object::MovingSphere(ref obj) => obj.aabb(point, time),
+            //   Object::BVHNode(ref obj) => obj.outward_normal(point, time),
+            // Object::Plane(ref obj) => obj.aabb(),
+        }
+    }
+}
+
 #[allow(dead_code)]
 #[derive(Clone, Debug)]
 pub struct Sphere {
@@ -163,288 +181,299 @@ impl Intersectable for Sphere {
     //     }
     // }
 }
-#[allow(dead_code)]
-#[derive(Clone, Debug)]
-pub struct MovingSphere {
-    pub center_0: Vec3,
-    pub center_1: Vec3,
-    pub time_0: f64,
-    pub time_1: f64,
-    pub radius: f64,
-    pub material: Material,
-}
 
-impl MovingSphere {
-    pub fn new(
-        center_0: Vec3,
-        center_1: Vec3,
-        time_0: f64,
-        time_1: f64,
-        radius: f64,
-        material: Material,
-    ) -> Object {
-        Object::MovingSphere(MovingSphere {
-            center_0,
-            center_1,
-            time_0,
-            time_1,
-            radius,
-            material,
-        })
-    }
-
-    pub fn center(&self, time: f64) -> Vec3 {
-        self.center_0
-            + ((time - self.time_0) / (self.time_1 - self.time_0)) * (self.center_1 - self.center_0)
+impl Bounded for Sphere {
+    fn aabb(&self) -> AABB {
+        let half_size = Vector3::new(self.radius, self.radius, self.radius);
+        let min = self.center - half_size;
+        let max = self.center + half_size;
+        AABB::with_bounds(min, max)
     }
 }
 
-impl Intersectable for MovingSphere {
-    fn intersects(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<f64> {
-        let oc = ray.origin - self.center(ray.time);
-        let a = ray.direction.norm();
-        let half_b = oc.dot(&ray.direction);
-        let c = oc.norm() - self.radius * self.radius;
-        let discriminant = half_b * half_b - a * c;
-
-        if discriminant < 0.0 {
-            return None;
-        }
-
-        let sqrtd = discriminant.sqrt();
-        let mut root = (-half_b - sqrtd) / a;
-        if root < t_min || t_max < root {
-            root = (-half_b + sqrtd) / a;
-            if root < t_min || t_max < root {
-                return None;
-            }
-        }
-        Some(root)
-    }
-
-    fn surface_normal(&self, point: &Vec3, _ray: &Ray) -> Vec3 {
-        (*point - self.center(0.0)).normalize()
-    }
-
-    fn outward_normal(&self, point: &Vec3, time: f64) -> Vec3 {
-        (*point - self.center(time)) / self.radius
-    }
-
-    fn surface_uv(&self, point: &Vec3) -> (f64, f64) {
-        let theta = (-point.y).acos();
-        let phi = (-point.z).atan2(point.x) + PI_F32;
-
-        (phi / (2.0 * PI_F32), theta / PI_F32)
-    }
-
-    // fn bounding_box(&self, time_0: f64, time_1: f64) -> AABB {
-    //     let box_0 = AABB::new(
-    //         self.center(time_0) - Vec3::new(self.radius, self.radius, self.radius),
-    //         self.center(time_0) + Vec3::new(self.radius, self.radius, self.radius),
-    //     );
-
-    //     let box_1 = AABB::new(
-    //         self.center(time_1) - Vec3::new(self.radius, self.radius, self.radius),
-    //         self.center(time_1) + Vec3::new(self.radius, self.radius, self.radius),
-    //     );
-
-    //     AABB::surrounding_box(box_0, box_1)
-    // }
-}
-#[allow(dead_code)]
-#[derive(Clone, Debug)]
-pub enum PlaneType {
-    YZ,
-    ZX,
-    XY,
-}
-
-#[allow(dead_code)]
-#[derive(Clone, Debug)]
-pub struct Plane {
-    plane_type: PlaneType,
-    a0: f64,
-    a1: f64,
-    b0: f64,
-    b1: f64,
-    k: f64,
-    material: Material,
-}
-
-impl Plane {
-    pub fn new(
-        plane_type: PlaneType,
-        a0: f64,
-        a1: f64,
-        b0: f64,
-        b1: f64,
-        k: f64,
-        material: Material,
-    ) -> Object {
-        Object::Plane(Plane {
-            plane_type,
-            a0,
-            a1,
-            b0,
-            b1,
-            k,
-            material,
-        })
-    }
-
-    pub fn get_axis(plane_type: &PlaneType) -> (usize, usize, usize) {
-        match plane_type {
-            PlaneType::YZ => (0, 1, 2),
-            PlaneType::ZX => (1, 2, 0),
-            PlaneType::XY => (2, 0, 1),
-        }
-    }
-}
-
-impl Intersectable for Plane {
-    fn intersects(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<f64> {
-        let (k_axis, a_axis, b_axis) = Plane::get_axis(&self.plane_type);
-
-        let ray_origin = ray.origin.to_vec();
-        let ray_dir = ray.direction.to_vec();
-
-        let t = (self.k - ray_origin[k_axis]) / ray_dir[k_axis];
-
-        if t < t_min || t > t_max {
-            None
-        } else {
-            let a = ray_origin[a_axis] + t * ray_dir[a_axis];
-            let b = ray_origin[b_axis] + t * ray_dir[b_axis];
-            if a < self.a0 || a > self.a1 || b < self.b0 || b > self.b1 {
-                None
-            } else {
-                Some(t)
-
-                // let u = (a - self.a0) / (self.a1 - self.a0);
-                // let v = (b - self.b0) / (self.b1 - self.b0);
-                // let p = ray.at(t);
-
-                // let mut normal = Vec3::zero().to_vec();
-                // normal[k_axis] = 1.0;
-
-                // Some(HitRecord {
-                //     t,
-                //     u,
-                //     v,
-                //     p,
-                //     normal,
-                //     material: &self.material,
-                // })
-            }
-        }
-    }
-
-    fn surface_normal(&self, _point: &Vec3, ray: &Ray) -> Vec3 {
-        let (k_axis, _a_axis, _b_axis) = Plane::get_axis(&self.plane_type);
-        // let p = point.to_vec();
-        let mut normal = Vec3::zero().to_vec();
-        normal[k_axis] = 1.0;
-
-        let p = ray.origin.to_vec();
-
-        if p[k_axis] > self.k {
-            normal[k_axis] = 1.0;
-        } else {
-            normal[k_axis] = -1.0;
-        }
-
-        //normal[k_axis] = 1.0;
-        Vec3::new(normal[0], normal[1], normal[2])
-    }
-
-    fn outward_normal(&self, point: &Vec3, _time: f64) -> Vec3 {
-        // let (k_axis, _a_axis, _b_axis) = Plane::get_axis(&self.plane_type);
-        // let mut normal = Vec3::zero().to_vec();
-        // normal[k_axis] = 1.0;
-
-        // Vec3::new(normal[0], normal[1], normal[2])
-        *point
-    }
-
-    fn surface_uv(&self, point: &Vec3) -> (f64, f64) {
-        let (_k_axis, a_axis, b_axis) = Plane::get_axis(&self.plane_type);
-
-        let point = point.to_vec();
-        let a = point[a_axis];
-        let b = point[b_axis];
-
-        (
-            (a - self.a0) / (self.a1 - self.a0),
-            (b - self.b0) / (self.b1 - self.b0),
-        )
-    }
-}
-
+// #[allow(dead_code)]
 // #[derive(Clone, Debug)]
-// pub struct Box {
-//     pub min: Vec3,
-//     pub max: Vec3,
-//     pub faces: Vec<Object>,
-//     pub default_mat: Material,
+// pub struct MovingSphere {
+//     pub center_0: Vec3,
+//     pub center_1: Vec3,
+//     pub time_0: f64,
+//     pub time_1: f64,
+//     pub radius: f64,
+//     pub material: Material,
 // }
 
-pub fn create_box(min: Vec3, max: Vec3, material: Material) -> Vec<Object> {
-    vec![
-        Plane::new(
-            PlaneType::XY,
-            min.x,
-            max.x,
-            min.y,
-            max.y,
-            max.z,
-            material.clone(),
-        ),
-        Plane::new(
-            PlaneType::XY,
-            min.x,
-            max.x,
-            min.y,
-            max.y,
-            min.z,
-            material.clone(),
-        ),
-        // bruh
-        Plane::new(
-            PlaneType::ZX,
-            min.z,
-            max.z,
-            min.x,
-            max.x,
-            max.y,
-            material.clone(),
-        ),
-        Plane::new(
-            PlaneType::ZX,
-            min.z,
-            max.z,
-            min.x,
-            max.x,
-            min.y,
-            material.clone(),
-        ),
-        // bruh
-        Plane::new(
-            PlaneType::YZ,
-            min.y,
-            max.y,
-            min.z,
-            max.z,
-            max.x,
-            material.clone(),
-        ),
-        Plane::new(
-            PlaneType::YZ,
-            min.y,
-            max.y,
-            min.z,
-            max.z,
-            min.x,
-            material.clone(),
-        ),
-    ]
-}
+// impl MovingSphere {
+//     pub fn new(
+//         center_0: Vec3,
+//         center_1: Vec3,
+//         time_0: f64,
+//         time_1: f64,
+//         radius: f64,
+//         material: Material,
+//     ) -> Object {
+//         Object::MovingSphere(MovingSphere {
+//             center_0,
+//             center_1,
+//             time_0,
+//             time_1,
+//             radius,
+//             material,
+//         })
+//     }
+
+//     pub fn center(&self, time: f64) -> Vec3 {
+//         self.center_0
+//             + ((time - self.time_0) / (self.time_1 - self.time_0)) * (self.center_1 - self.center_0)
+//     }
+// }
+
+// impl Intersectable for MovingSphere {
+//     fn intersects(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<f64> {
+//         let oc = ray.origin - self.center(ray.time);
+//         let a = ray.direction.norm();
+//         let half_b = oc.dot(&ray.direction);
+//         let c = oc.norm() - self.radius * self.radius;
+//         let discriminant = half_b * half_b - a * c;
+
+//         if discriminant < 0.0 {
+//             return None;
+//         }
+
+//         let sqrtd = discriminant.sqrt();
+//         let mut root = (-half_b - sqrtd) / a;
+//         if root < t_min || t_max < root {
+//             root = (-half_b + sqrtd) / a;
+//             if root < t_min || t_max < root {
+//                 return None;
+//             }
+//         }
+//         Some(root)
+//     }
+
+//     fn surface_normal(&self, point: &Vec3, _ray: &Ray) -> Vec3 {
+//         (*point - self.center(0.0)).normalize()
+//     }
+
+//     fn outward_normal(&self, point: &Vec3, time: f64) -> Vec3 {
+//         (*point - self.center(time)) / self.radius
+//     }
+
+//     fn surface_uv(&self, point: &Vec3) -> (f64, f64) {
+//         let theta = (-point.y).acos();
+//         let phi = (-point.z).atan2(point.x) + PI_F32;
+
+//         (phi / (2.0 * PI_F32), theta / PI_F32)
+//     }
+
+// fn bounding_box(&self, time_0: f64, time_1: f64) -> AABB {
+//     let box_0 = AABB::new(
+//         self.center(time_0) - Vec3::new(self.radius, self.radius, self.radius),
+//         self.center(time_0) + Vec3::new(self.radius, self.radius, self.radius),
+//     );
+
+//     let box_1 = AABB::new(
+//         self.center(time_1) - Vec3::new(self.radius, self.radius, self.radius),
+//         self.center(time_1) + Vec3::new(self.radius, self.radius, self.radius),
+//     );
+
+//     AABB::surrounding_box(box_0, box_1)
+// }
+// }
+
+// #[allow(dead_code)]
+// #[derive(Clone, Debug)]
+// pub enum PlaneType {
+//     YZ,
+//     ZX,
+//     XY,
+// }
+
+// #[allow(dead_code)]
+// #[derive(Clone, Debug)]
+// pub struct Plane {
+//     plane_type: PlaneType,
+//     a0: f64,
+//     a1: f64,
+//     b0: f64,
+//     b1: f64,
+//     k: f64,
+//     material: Material,
+// }
+
+// impl Plane {
+//     pub fn new(
+//         plane_type: PlaneType,
+//         a0: f64,
+//         a1: f64,
+//         b0: f64,
+//         b1: f64,
+//         k: f64,
+//         material: Material,
+//     ) -> Object {
+//         Object::Plane(Plane {
+//             plane_type,
+//             a0,
+//             a1,
+//             b0,
+//             b1,
+//             k,
+//             material,
+//         })
+//     }
+
+//     pub fn get_axis(plane_type: &PlaneType) -> (usize, usize, usize) {
+//         match plane_type {
+//             PlaneType::YZ => (0, 1, 2),
+//             PlaneType::ZX => (1, 2, 0),
+//             PlaneType::XY => (2, 0, 1),
+//         }
+//     }
+// }
+
+// impl Intersectable for Plane {
+//     fn intersects(&self, ray: &Ray, t_min: f64, t_max: f64) -> Option<f64> {
+//         let (k_axis, a_axis, b_axis) = Plane::get_axis(&self.plane_type);
+
+//         let ray_origin = ray.origin.to_vec();
+//         let ray_dir = ray.direction.to_vec();
+
+//         let t = (self.k - ray_origin[k_axis]) / ray_dir[k_axis];
+
+//         if t < t_min || t > t_max {
+//             None
+//         } else {
+//             let a = ray_origin[a_axis] + t * ray_dir[a_axis];
+//             let b = ray_origin[b_axis] + t * ray_dir[b_axis];
+//             if a < self.a0 || a > self.a1 || b < self.b0 || b > self.b1 {
+//                 None
+//             } else {
+//                 Some(t)
+
+//                 // let u = (a - self.a0) / (self.a1 - self.a0);
+//                 // let v = (b - self.b0) / (self.b1 - self.b0);
+//                 // let p = ray.at(t);
+
+//                 // let mut normal = Vec3::zero().to_vec();
+//                 // normal[k_axis] = 1.0;
+
+//                 // Some(HitRecord {
+//                 //     t,
+//                 //     u,
+//                 //     v,
+//                 //     p,
+//                 //     normal,
+//                 //     material: &self.material,
+//                 // })
+//             }
+//         }
+//     }
+
+//     fn surface_normal(&self, _point: &Vec3, ray: &Ray) -> Vec3 {
+//         let (k_axis, _a_axis, _b_axis) = Plane::get_axis(&self.plane_type);
+//         // let p = point.to_vec();
+//         let mut normal = Vec3::zero().to_vec();
+//         normal[k_axis] = 1.0;
+
+//         let p = ray.origin.to_vec();
+
+//         if p[k_axis] > self.k {
+//             normal[k_axis] = 1.0;
+//         } else {
+//             normal[k_axis] = -1.0;
+//         }
+
+//         //normal[k_axis] = 1.0;
+//         Vec3::new(normal[0], normal[1], normal[2])
+//     }
+
+//     fn outward_normal(&self, point: &Vec3, _time: f64) -> Vec3 {
+//         // let (k_axis, _a_axis, _b_axis) = Plane::get_axis(&self.plane_type);
+//         // let mut normal = Vec3::zero().to_vec();
+//         // normal[k_axis] = 1.0;
+
+//         // Vec3::new(normal[0], normal[1], normal[2])
+//         *point
+//     }
+
+//     fn surface_uv(&self, point: &Vec3) -> (f64, f64) {
+//         let (_k_axis, a_axis, b_axis) = Plane::get_axis(&self.plane_type);
+
+//         let point = point.to_vec();
+//         let a = point[a_axis];
+//         let b = point[b_axis];
+
+//         (
+//             (a - self.a0) / (self.a1 - self.a0),
+//             (b - self.b0) / (self.b1 - self.b0),
+//         )
+//     }
+// }
+
+// // #[derive(Clone, Debug)]
+// // pub struct Box {
+// //     pub min: Vec3,
+// //     pub max: Vec3,
+// //     pub faces: Vec<Object>,
+// //     pub default_mat: Material,
+// // }
+
+// pub fn create_box(min: Vec3, max: Vec3, material: Material) -> Vec<Object> {
+//     vec![
+//         Plane::new(
+//             PlaneType::XY,
+//             min.x,
+//             max.x,
+//             min.y,
+//             max.y,
+//             max.z,
+//             material.clone(),
+//         ),
+//         Plane::new(
+//             PlaneType::XY,
+//             min.x,
+//             max.x,
+//             min.y,
+//             max.y,
+//             min.z,
+//             material.clone(),
+//         ),
+//         // bruh
+//         Plane::new(
+//             PlaneType::ZX,
+//             min.z,
+//             max.z,
+//             min.x,
+//             max.x,
+//             max.y,
+//             material.clone(),
+//         ),
+//         Plane::new(
+//             PlaneType::ZX,
+//             min.z,
+//             max.z,
+//             min.x,
+//             max.x,
+//             min.y,
+//             material.clone(),
+//         ),
+//         // bruh
+//         Plane::new(
+//             PlaneType::YZ,
+//             min.y,
+//             max.y,
+//             min.z,
+//             max.z,
+//             max.x,
+//             material.clone(),
+//         ),
+//         Plane::new(
+//             PlaneType::YZ,
+//             min.y,
+//             max.y,
+//             min.z,
+//             max.z,
+//             min.x,
+//             material.clone(),
+//         ),
+//     ]
+// }
